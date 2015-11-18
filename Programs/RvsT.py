f46cfe341084a0d3909c5b2a13c927ba914d6e9d
ejetzer <ejetzer@gmail.com>
2015-11-18 00:19:45 -0500

    Worked on error analysis and magnet calibration.

--- Programs/RvsT.py
+++ Programs/RvsT.py
@@ -3,37 +3,42 @@
 
 import spinmob, glob, sys, matplotlib.pylab as pylab, scipy
 
-def fit_exp(Ts, Rs, Rerr):
+def fit_exp(Ts, Rs, Rerr, eguess):
     'Fit an exponential function to the data'
-    model, ps = 'a * exp(b * (x-x0))', 'a,b,x0'
+    model, ps = 'a * exp(b * x)', 'a,b'
     # Make intelligent guesses for the parameters
-    a, b, x0 = 1, 1, 0
+    a, b = eguess
+    b = pylab.log(Rs[-1]/Rs[0]) / ( Ts[-1] - Ts[0] )
     # Create a spinmob fitter
     fitter = spinmob.data.fitter(model, ps)
     fitter.set_data(Ts, Rs, Rerr)
-    fitter.set(a=a, b=b, x0=x0) # Set guesses
+    fitter.set(a=a, b=b) # Set guesses
+    fitter.set(xlabel='Temperature',
+               ylabel='Resistance')
     # Fit.
     fitter.fit()
     pylab.savefig('../Graphs/RvsT/fit_exp.png')
     pylab.savefig('../Graphs/RvsT/fit_exp.pdf')
     return fitter
 
-def fit_power(Ts, Rs, Rerr):
+def fit_power(Ts, Rs, Rerr, pguess):
     'Fit a power function to the data'
     model, ps = 'a * (x-x0)**(3/2)', 'a,x0'
     # Make intelligent guesses for the parameters
-    a, x0 = 1, 1
+    a, x0 = pguess
     # Create a spinmob fitter
     fitter = spinmob.data.fitter(model, ps)
     fitter.set_data(Ts, Rs, Rerr)
     fitter.set(a=a, x0=x0) # Set guesses
+    fitter.set(xlabel='Temperature',
+               ylabel='Resistance')
     # Fit.
     fitter.fit()
     pylab.savefig('../Graphs/RvsT/fit_power.png')
     pylab.savefig('../Graphs/RvsT/fit_power.pdf')
     return fitter
 
-def splitfit(Ts, Rs, es, a, b, c, d):
+def splitfit(Ts, Rs, es, a, b, c, d, pguess, eguess):
     ## Split the data in two parts
     x1, x2, y1, y2, e1, e2 = [], [], [], [], [], []
     for T, R, e in zip(Ts, Rs, es):
@@ -46,12 +51,12 @@ def splitfit(Ts, Rs, es, a, b, c, d):
             y2.append(abs(R))
             e2.append(e)
     ## Fit one part with the exponential
-    fit1 = fit_power(x1, y1, e1)
+    fit1 = fit_power(x1, y1, e1, pguess)
     ## Fit one part with the polynomial
-    fit2 = fit_exp(x2, y2, e2)
+    fit2 = fit_exp(x2, y2, e2, eguess)
     return fit1, fit2
 
-def main(data_file, a, b, c, d):
+def main(data_file, a, b, c, d, pguess, eguess):
     databox = spinmob.data.load(data_file)
     xs, ys, es = databox[0], databox[1], databox[2]
     if 'current' in databox.hkeys:
@@ -61,7 +66,7 @@ def main(data_file, a, b, c, d):
         databox.h(current=current)
     Rs = ys / current
     Rerrs = es / current
-    fits = splitfit(xs, Rs, Rerrs, a, b, c, d)
+    fits = splitfit(xs, Rs, Rerrs, a, b, c, d, pguess, eguess)
     return fits
 
 if __name__ == '__main__':
